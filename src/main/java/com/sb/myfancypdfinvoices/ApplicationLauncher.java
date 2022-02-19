d43211a042d9f250acb77987029c62baa7194e21
Shitij Bhatnagar <shitijbhatnagar@yahoo.com>
2022-02-19 12:30:43 +0530

    text changes

--- src/main/java/com/sb/myfancypdfinvoices/ApplicationLauncher.java
+++ src/main/java/com/sb/myfancypdfinvoices/ApplicationLauncher.java
@@ -21,6 +21,6 @@ public class ApplicationLauncher {
 
         tomcat.start();
 
-        System.out.println("ApplicationLauncher: Tomcat Server has been started");
+        System.out.println("ApplicationLauncher: Tomcat Server has been started and is running");
     }
 }
